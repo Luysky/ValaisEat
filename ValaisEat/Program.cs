249018d69ec3002a76925d52fe22324927111cbb
Luysky <56729035+Luysky@users.noreply.github.com>
2019-11-02 12:16:35 +0100

    Test

--- ValaisEat/Program.cs
+++ ValaisEat/Program.cs
@@ -7,6 +7,8 @@ namespace ValaisEat
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("C'est bien moi !");
         }
     }
 }
