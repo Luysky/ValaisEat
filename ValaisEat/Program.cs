dfa167aca890add583a07cd327d2e692c7b779e0
Luysky <56729035+Luysky@users.noreply.github.com>
2019-11-04 12:06:17 +0100

    Mise en place de la structure

--- ValaisEat/Program.cs
+++ ValaisEat/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+
+
 
 namespace ValaisEat
 {
     class Program
     {
+     
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
