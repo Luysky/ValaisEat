84f1e25fe2c24b3dd94ee415734242bb93a0aa48
Sepkohn <50259520+Sepkohn@users.noreply.github.com>
2019-11-05 23:15:57 +0100

    Orders, orderDishes et restaurant a finir

---
+++ DAL/RestaurantsDB.cs
@@ -0,0 +1,181 @@
+﻿using DTO;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DAL
+{
+    class RestaurantsDB
+    {
+
+        public IConfiguration Configuration { get; }
+        public RestaurantsDB(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public List<Area> GetAreas()
+        {
+            List<Area> results = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Areas";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            if (results == null)
+                                results = new List<Area>();
+
+                            Area areas = new Area();
+
+                            areas.IdArea = (int)dr["IdArea"];
+                            areas.Name = (string)dr["Name"];
+                            areas.IdCountry = (int)dr["IdCountry"];
+
+
+                            results.Add(areas);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return results;
+        }
+
+        public Area GetArea(int id)
+        {
+            Area area = null;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Areas WHERE IdArea = @id";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            area = new Area();
+
+                            area.IdArea = (int)dr["IdArea"];
+                            area.Name = (string)dr["Name"];
+                            area.IdCountry = (int)dr["IdCountry"];
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return area;
+        }
+
+        public Area AddArea(Area area)
+        {
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "INSERT INTO Areas(name, idcountry) VALUES(@name, @idcountry); SELECT SCOPE_IDENTITY()";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@name", area.Name);
+                    cmd.Parameters.AddWithValue("@idcountry", area.IdCountry);
+
+
+                    cn.Open();
+
+                    area.IdArea = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return area;
+        }
+
+        public int UpdateArea(Area area)
+        {
+            int result = 0;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "UPDATE Areas SET idArea=@idArea, name=@name, idCountry=@idCountry WHERE idArea=@id";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@idArea", area.IdArea);
+                    cmd.Parameters.AddWithValue("@name", area.Name);
+                    cmd.Parameters.AddWithValue("@idCountry", area.IdCountry);
+
+
+                    cn.Open();
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return result;
+        }
+
+        public int DeleteArea(int id)
+        {
+            int result = 0;
+            string connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Areas WHERE idArea=@id";
+                    SqlCommand cmd = new SqlCommand(query, cn);
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cn.Open();
+
+                    result = cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
