d603b238af5b4797d9298359f4234a3dd08a9ee1
Luysky <56729035+Luysky@users.noreply.github.com>
2019-11-05 09:45:58 +0100

    Juste pour un pull

--- ValaisEat/Program.cs
+++ ValaisEat/Program.cs
@@ -11,7 +11,7 @@ namespace ValaisEat
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!");
 
             Console.WriteLine("C'est bien moi !");
 
