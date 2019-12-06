f71581d9000283d47b3bc4141f4c85c80baa54b1
Sepkohn <50259520+Sepkohn@users.noreply.github.com>
2019-12-06 16:02:03 +0100

    Web

--- BLL/IAreasManager.cs
+++ BLL/IAreasManager.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace BLL
 {
-    internal interface IAreasManager
+    public interface IAreasManager
     {
         IAreasDB AreasDbObject { get; }
 
---
+++ WebApplication1/Controllers/AreaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BLL;
+
+namespace WebApplication1.Controllers
+{
+    public class AreaController : Controller
+    {
+
+        private IAreasManager AreaManager { get; }
+        public AreaController(IAreasManager areasManager)
+        {
+            AreaManager = areasManager;
+        }
+
+        // GET: Area
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Area/Details/5
+        public ActionResult Select(int id)
+        {
+            return View();
+        }
+
+        public ActionResult GetAllAreas() {
+
+            var areas = AreaManager.GetAreas();
+
+            return View(areas);
+        }
+
+    }
+}
